fb712ee97744442852e424246855e0437a9463b9
Enzo AICARDI <eaicardi@datasolution.fr>
2024-11-14 11:05:04 +0100

    [tests] add dependencies test

--- tests/reactivity.test.ts
+++ tests/reactivity.test.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "bun:test";
 import { Reactive, Signal } from "../src/@enzoaicardi/reactivity";
+import { Symbols } from "../src/@enzoaicardi/reactivity/symbols";
 
 test("signal: set, get, compute", () => {
     // init
@@ -26,4 +27,28 @@ test("reactive: set, get, compute", () => {
     // compute
     signal.compute((value) => (value += 1));
     expect(number).toBe(2);
+});
+
+test("dependencies: auto, add, delete, clear", () => {
+    const signalAuto = new Signal(0);
+    const signalManual = new Signal(0);
+    const reactive = new Reactive(() => signalAuto.get());
+    reactive.use();
+    // auto
+    expect(reactive[Symbols.dependencies].size).toBe(1);
+    expect(signalAuto[Symbols.dependencies].size).toBe(1);
+    // add
+    reactive.add(signalManual);
+    expect(reactive[Symbols.dependencies].size).toBe(2);
+    expect(signalManual[Symbols.dependencies].size).toBe(1);
+    // delete
+    reactive.delete(signalAuto);
+    expect(reactive[Symbols.dependencies].size).toBe(1);
+    expect(signalAuto[Symbols.dependencies].size).toBe(0);
+    // clear
+    signalManual.clear();
+    expect(reactive[Symbols.dependencies].size).toBe(0);
+    reactive.add(signalManual);
+    reactive.clear();
+    expect(reactive[Symbols.dependencies].size).toBe(0);
 });
